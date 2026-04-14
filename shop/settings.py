ad3b51edf4ea20f421b995c91a1bf77e1920a3d1
Yoan Ivanov <yoan.ivanov13@gmail.com>
2026-04-14 15:44:57 +0300

    Added configuration changes

--- shop/settings.py
+++ shop/settings.py
@@ -30,6 +30,10 @@ DEBUG = config('DEBUG', None, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', "").split(",")
 
 
+SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default=False, cast=bool)
+SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", default=False, cast=bool)
+CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", default=False, cast=bool)
+
 # Application definition
 
 PROJECT_APPS = [
